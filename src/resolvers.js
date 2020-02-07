f3330a3171563df639b96000f44c64193d25a132
Rickson <rickson.simoes@hotmail.com>
2020-02-07 15:51:54 -0300

    :hammer: Reparos e formatacao

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,26 +3,35 @@ const Contas = require('./Conta');
 module.exports = {
   Query: {
     contas: () => Contas.find(),
-    saldo: (_, { conta }) => Contas.findOne({conta})
+    saldo: (_, { conta }) => Contas.findOne({ conta })
   },
 
   Mutation: {
-    criarConta: (_, {conta, saldo}) => Contas.create({conta, saldo}),
-    depositar: (_, {conta, valor}) => Contas.findOneAndUpdate({conta}, { $inc: { saldo: valor }}).exec(),
+    criarConta: (_, {conta, saldo}) => Contas.create({ conta, saldo }),
+
+    depositar: async (_, {conta, valor}) => {      
+      var { conta } = await Contas.findOne({ conta });      
+
+      const insercaoValor = await Contas.findOneAndUpdate({ conta }, { 
+        $inc: { saldo: valor }
+      });
+      
+      return insercaoValor;
+    },
+
     sacar: async (_, {conta, valor}) => {
+      var { conta, saldo } = await Contas.findOne({ conta });        
 
-      var MenorQue = await Contas.findOne({conta});      
-
-      if ( (MenorQue.saldo-valor) < 0 ) {
+      if ((saldo-valor) < 0 ) {
         throw new Error(
           "Não pode sacar esta quantia"
-        )
-      }
-      
-     const insercaoValor = await Contas.findOneAndUpdate({conta}, { $inc: { saldo: - valor }}).exec();
+        )};
 
-     return insercaoValor;
+      const insercaoValor = await Contas.findOneAndUpdate({ conta }, { 
+       $inc: { saldo: - valor }
+      });
 
+      return insercaoValor;
     },
   },
 }
